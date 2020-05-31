d0748a5baf868b21852d6a4d194352f46ea75854
nish.gowda6@gmail.com <60044986+nishgowda@users.noreply.github.com>
2020-05-31 11:32:32 -0400

    Adding user authentication

--- server/main.go
+++ server/main.go
@@ -21,6 +21,7 @@ type Issues struct {
 	Description string
 	Priority    string
 	Date        string
+	User_id     int
 }
 
 type Users struct {
@@ -32,7 +33,7 @@ type Users struct {
 type Projects struct {
 	Id          int
 	UserId      int
-	Name        string
+	ProjectName string
 	Description string
 }
 
@@ -76,13 +77,13 @@ func dbConn() (db *sql.DB) {
 var tmpl = template.Must(template.ParseGlob("../views/*"))
 var uid int
 var project_id int
+var singedIn = false
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	tmpl.ExecuteTemplate(w, "Login", nil)
 }
 
 func Login(w http.ResponseWriter, r *http.Request) {
-
 	db := dbConn()
 	if r.Method == "POST" {
 		r.ParseForm()
@@ -105,7 +106,9 @@ func Login(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Succesfully logged in as " + username)
 	}
 	defer db.Close()
+
 	fmt.Println(uid)
+
 	http.Redirect(w, r, "/dashboard", 301)
 }
 
@@ -135,186 +138,258 @@ func Register(w http.ResponseWriter, r *http.Request) {
 }
 
 func Dashboard(w http.ResponseWriter, r *http.Request) {
-	db := dbConn()
-	selDB, err := db.Query("SELECT * FROM Projects WHERE user_id=? ORDER BY id DESC", uid)
-	if err != nil {
-		panic(err.Error())
-	}
-	emp := Projects{}
-	res := []Projects{}
-	for selDB.Next() {
-		var id, user_id int
-		var name, description string
-		err = selDB.Scan(&id, &name, &description, &user_id)
+	if uid != 0 {
+		db := dbConn()
+		selDB, err := db.Query("SELECT * FROM Projects WHERE user_id=? ORDER BY id DESC", uid)
 		if err != nil {
 			panic(err.Error())
 		}
-		emp.Id = id
-		emp.UserId = user_id
-		emp.Name = name
-		emp.Description = description
-		res = append(res, emp)
+		emp := Projects{}
+		res := []Projects{}
+		for selDB.Next() {
+			var id, user_id int
+			var name, description string
+			err = selDB.Scan(&id, &name, &description, &user_id)
+			if err != nil {
+				panic(err.Error())
+			}
+			emp.Id = id
+			emp.UserId = user_id
+			emp.ProjectName = name
+			emp.Description = description
+			res = append(res, emp)
+		}
+		fmt.Println(uid)
+		tmpl.ExecuteTemplate(w, "Dashboard", res)
+		defer db.Close()
+	} else {
+		http.Redirect(w, r, "/", 301)
 	}
-	fmt.Println(uid)
-	tmpl.ExecuteTemplate(w, "Dashboard", res)
-	defer db.Close()
 }
+
 func NewProject(w http.ResponseWriter, r *http.Request) {
 	tmpl.ExecuteTemplate(w, "NewProject", nil)
 }
 func InsertProject(w http.ResponseWriter, r *http.Request) {
-	db := dbConn()
-	if r.Method == "POST" {
-		r.ParseForm()
-		name, description := r.PostFormValue("name"), r.PostFormValue("description")
-		insForm, err := db.Prepare("INSERT INTO Projects(name, description, user_id) VALUES(?,?,?)")
-		if err != nil {
-			panic(err.Error())
+	if uid != 0 {
+		db := dbConn()
+		if r.Method == "POST" {
+			r.ParseForm()
+			name, description := r.PostFormValue("name"), r.PostFormValue("description")
+			insForm, err := db.Prepare("INSERT INTO Projects(name, description, user_id) VALUES(?,?,?)")
+			if err != nil {
+				panic(err.Error())
+			}
+			insForm.Exec(name, description, uid)
+			log.Println("INSERT: Name: " + name + " | Description: " + description)
 		}
-		insForm.Exec(name, description, uid)
-		log.Println("INSERT: Name: " + name + " | Description: " + description)
+		defer db.Close()
+		http.Redirect(w, r, "/dashboard", 301)
+	} else {
+		http.Redirect(w, r, "/", 301)
 	}
-	defer db.Close()
-	http.Redirect(w, r, "/dashboard", 301)
 }
 
 func ShowProject(w http.ResponseWriter, r *http.Request) {
-	db := dbConn()
-	nId := r.URL.Query().Get("id")
-	selDB, err := db.Query("SELECT * FROM Projects WHERE id=? and user_id=?", nId, uid)
-	if err != nil {
-		panic(err.Error())
-	}
-	emp := Projects{}
-	for selDB.Next() {
-		var id, user_id int
-		var name, description string
-		err = selDB.Scan(&id, &name, &description, &user_id)
+	if uid != 0 {
+		db := dbConn()
+		nId := r.URL.Query().Get("id")
+		selDB, err := db.Query("SELECT * FROM Projects WHERE id=? and user_id=?", nId, uid)
 		if err != nil {
 			panic(err.Error())
 		}
-		emp.Id = id
-		emp.UserId = user_id
-		emp.Name = name
-		emp.Description = description
-		project_id = emp.Id
-		fmt.Println("Project id is " + string(project_id))
+		emp := Projects{}
+		for selDB.Next() {
+			var id, user_id int
+			var name, description string
+			err = selDB.Scan(&id, &name, &description, &user_id)
+			if err != nil {
+				panic(err.Error())
+			}
+			emp.Id = id
+			emp.UserId = user_id
+			emp.ProjectName = name
+			emp.Description = description
+			project_id = emp.Id
+			fmt.Println("Project id is " + string(project_id))
+		}
+		tmpl.ExecuteTemplate(w, "ShowProject", emp)
+		defer db.Close()
+	} else {
+		http.Redirect(w, r, "/", 301)
 	}
-	tmpl.ExecuteTemplate(w, "ShowProject", emp)
-	defer db.Close()
 }
 
 func EditProject(w http.ResponseWriter, r *http.Request) {
-	db := dbConn()
-	nId := r.URL.Query().Get("id")
-	fmt.Println(r.Method)
-	selDB, err := db.Query("SELECT * FROM Projects WHERE id=? and user_id=?", nId, uid)
-	if err != nil {
-		panic(err.Error())
-	}
-	emp := Projects{}
-	for selDB.Next() {
-		var id, user_id int
-		var name, description string
-		err = selDB.Scan(&id, &name, &description, &user_id)
+	if uid != 0 {
+		db := dbConn()
+		nId := r.URL.Query().Get("id")
+		fmt.Println(r.Method)
+		selDB, err := db.Query("SELECT * FROM Projects WHERE id=? and user_id=?", nId, uid)
 		if err != nil {
 			panic(err.Error())
 		}
-		emp.Id = id
-		emp.UserId = user_id
-		emp.Name = name
-		emp.Description = description
+		emp := Projects{}
+		for selDB.Next() {
+			var id, user_id int
+			var name, description string
+			err = selDB.Scan(&id, &name, &description, &user_id)
+			if err != nil {
+				panic(err.Error())
+			}
+			emp.Id = id
+			emp.UserId = user_id
+			emp.ProjectName = name
+			emp.Description = description
+		}
+		tmpl.ExecuteTemplate(w, "EditProject", emp)
+		defer db.Close()
+	} else {
+		http.Redirect(w, r, "/", 301)
 	}
-	tmpl.ExecuteTemplate(w, "EditProject", emp)
-	defer db.Close()
 }
 
 func UpdateProject(w http.ResponseWriter, r *http.Request) {
-	db := dbConn()
-	if r.Method == "POST" {
-		r.ParseForm()
-		name, description := r.PostFormValue("name"), r.PostFormValue("description")
-		id := r.FormValue("uid")
-		insForm, err := db.Prepare("UPDATE Projects SET name=?, description=? WHERE id=?")
-		if err != nil {
-			fmt.Println(err.Error())
+	if uid != 0 {
+		db := dbConn()
+		if r.Method == "POST" {
+			r.ParseForm()
+			name, description := r.PostFormValue("name"), r.PostFormValue("description")
+			id := r.FormValue("uid")
+			insForm, err := db.Prepare("UPDATE Projects SET name=?, description=? WHERE id=?")
+			if err != nil {
+				fmt.Println(err.Error())
+			}
+			insForm.Exec(name, description, id)
+			log.Println("UPDATE: Name: " + name + " | Description: " + description + " | Priority: ")
 		}
-		insForm.Exec(name, description, id)
-		log.Println("UPDATE: Name: " + name + " | Description: " + description + " | Priority: ")
+		defer db.Close()
+		http.Redirect(w, r, "/dashboard", 301)
+	} else {
+		http.Redirect(w, r, "/", 301)
 	}
-	defer db.Close()
-	http.Redirect(w, r, "/dashboard", 301)
+
 }
 func DeleteProject(w http.ResponseWriter, r *http.Request) {
-	db := dbConn()
-	emp := r.URL.Query().Get("id")
-	fmt.Println(r.Method)
-	fmt.Println(emp)
-	delForm, err := db.Prepare("DELETE FROM Projects WHERE id=?")
-	if err != nil {
-		panic(err.Error())
+	if uid != 0 {
+		db := dbConn()
+		emp := r.URL.Query().Get("id")
+		fmt.Println(r.Method)
+		fmt.Println(emp)
+		delForm, err := db.Prepare("DELETE FROM Projects WHERE id=?")
+		if err != nil {
+			panic(err.Error())
+		}
+		delForm.Exec(emp)
+		log.Println("DELETE")
+		defer db.Close()
+		http.Redirect(w, r, "/dashboard", 301)
+	} else {
+		http.Redirect(w, r, "/", 301)
 	}
-	delForm.Exec(emp)
-	log.Println("DELETE")
-	defer db.Close()
-	http.Redirect(w, r, "/dashboard", 301)
+}
+
+func DisplayIssues(w http.ResponseWriter, r *http.Request) {
+	if uid != 0 {
+		db := dbConn()
+		selDB, err := db.Query("SELECT * FROM Issues WHERE user_id=? ORDER BY id DESC", uid)
+		if err != nil {
+			panic(err.Error())
+		}
+		emp := Issues{}
+		res := []Issues{}
+		for selDB.Next() {
+			var id, user_id int
+			var name, description, priority, date string
+			err = selDB.Scan(&id, &name, &description, &priority, &date, &project_id, &user_id)
+			if err != nil {
+				panic(err.Error())
+			}
+			emp.Id = id
+			emp.User_id = user_id
+			emp.Name = name
+			emp.Description = description
+			emp.Priority = priority
+			emp.Date = date
+			res = append(res, emp)
+		}
+		fmt.Print("display users id is ")
+		fmt.Println(uid)
+		tmpl.ExecuteTemplate(w, "Issues", res)
+		defer db.Close()
+	} else {
+		http.Redirect(w, r, "/", 301)
+	}
+
 }
 
 //Index routes to index template, returns all available issues
 func Index(w http.ResponseWriter, r *http.Request) {
-	db := dbConn()
-	selDB, err := db.Query("SELECT * FROM Issues WHERE project_id=? ORDER BY id DESC", project_id)
-	if err != nil {
-		panic(err.Error())
-	}
-	emp := Issues{}
-	res := []Issues{}
-	for selDB.Next() {
-		var id, project_id int
-		var name, description, priority, date string
-		err = selDB.Scan(&id, &name, &description, &priority, &date, &project_id)
+	if uid != 0 {
+		db := dbConn()
+		selDB, err := db.Query("SELECT * FROM Issues WHERE project_id=? and user_id=? ORDER BY id DESC", project_id, uid)
 		if err != nil {
 			panic(err.Error())
 		}
-		emp.Id = id
-		emp.Project_id = project_id
-		emp.Name = name
-		emp.Description = description
-		emp.Priority = priority
-		emp.Date = date
-		res = append(res, emp)
+		emp := Issues{}
+		res := []Issues{}
+		for selDB.Next() {
+			var id, project_id, user_id int
+			var name, description, priority, date string
+			err = selDB.Scan(&id, &name, &description, &priority, &date, &project_id, &user_id)
+			if err != nil {
+				panic(err.Error())
+			}
+			emp.Id = id
+			emp.Project_id = project_id
+			emp.User_id = user_id
+			emp.Name = name
+			emp.Description = description
+			emp.Priority = priority
+			emp.Date = date
+			res = append(res, emp)
+		}
+		fmt.Print("user id is ")
+		fmt.Println(uid)
+		tmpl.ExecuteTemplate(w, "Index", res)
+		defer db.Close()
+	} else {
+		fmt.Println(uid)
+		http.Redirect(w, r, "/", 301)
 	}
-	fmt.Println(uid)
-	tmpl.ExecuteTemplate(w, "Index", res)
-	defer db.Close()
 }
 
 // Show is a function that routes to View template
 func Show(w http.ResponseWriter, r *http.Request) {
-	db := dbConn()
-	nId := r.URL.Query().Get("id")
-	fmt.Println(nId)
-	selDB, err := db.Query("SELECT * FROM Issues WHERE id=? and project_id=?", nId, project_id)
-	if err != nil {
-		panic(err.Error())
-	}
-	emp := Issues{}
-	for selDB.Next() {
-		var id, project_id int
-		var name, description, priority, date string
-		err = selDB.Scan(&id, &name, &description, &priority, &date, &project_id)
+	if uid != 0 {
+		db := dbConn()
+		nId := r.URL.Query().Get("id")
+		fmt.Println(nId)
+		selDB, err := db.Query("SELECT * FROM Issues WHERE id=? and project_id=? and user_id=?", nId, project_id, uid)
 		if err != nil {
 			panic(err.Error())
 		}
-		emp.Id = id
-		emp.Project_id = project_id
-		emp.Name = name
-		emp.Description = description
-		emp.Priority = priority
-		emp.Date = date
+		emp := Issues{}
+		for selDB.Next() {
+			var id, project_id, user_id int
+			var name, description, priority, date string
+			err = selDB.Scan(&id, &name, &description, &priority, &date, &project_id, &user_id)
+			if err != nil {
+				panic(err.Error())
+			}
+			emp.Id = id
+			emp.Project_id = project_id
+			emp.User_id = user_id
+			emp.Name = name
+			emp.Description = description
+			emp.Priority = priority
+			emp.Date = date
+		}
+		tmpl.ExecuteTemplate(w, "Show", emp)
+		defer db.Close()
+	} else {
+		http.Redirect(w, r, "/", 301)
 	}
-	tmpl.ExecuteTemplate(w, "Show", emp)
-	defer db.Close()
 }
 
 // New is a router to create a new Blip
@@ -324,82 +399,99 @@ func New(w http.ResponseWriter, r *http.Request) {
 
 // Edit is a route to UPDATE an existing Blip
 func Edit(w http.ResponseWriter, r *http.Request) {
-	db := dbConn()
-	nId := r.URL.Query().Get("id")
-	fmt.Println(r.Method)
-	selDB, err := db.Query("SELECT * FROM Issues WHERE id=? and project_id=?", nId, project_id)
-	if err != nil {
-		panic(err.Error())
-	}
-	emp := Issues{}
-	for selDB.Next() {
-		var id, project_id int
-		var name, description, priority, date string
-		err = selDB.Scan(&id, &name, &description, &priority, &date, &project_id)
+	if uid != 0 {
+		db := dbConn()
+		nId := r.URL.Query().Get("id")
+		fmt.Println(r.Method)
+		selDB, err := db.Query("SELECT * FROM Issues WHERE id=? and project_id=? and user_id", nId, project_id, uid)
 		if err != nil {
 			panic(err.Error())
 		}
-		emp.Id = id
-		emp.Project_id = project_id
-		emp.Name = name
-		emp.Description = description
-		emp.Priority = priority
-		emp.Date = date
+		emp := Issues{}
+		for selDB.Next() {
+			var id, project_id int
+			var name, description, priority, date string
+			err = selDB.Scan(&id, &name, &description, &priority, &date, &project_id, &uid)
+			if err != nil {
+				panic(err.Error())
+			}
+			emp.Id = id
+			emp.Project_id = project_id
+			emp.Name = name
+			emp.Description = description
+			emp.Priority = priority
+			emp.Date = date
+		}
+		tmpl.ExecuteTemplate(w, "Edit", emp)
+		defer db.Close()
+	} else {
+		fmt.Println(uid)
+		http.Redirect(w, r, "/", 301)
 	}
-	tmpl.ExecuteTemplate(w, "Edit", emp)
-	defer db.Close()
 }
 
 // Insert is a router function that creates the new Blip
 func Insert(w http.ResponseWriter, r *http.Request) {
-	db := dbConn()
-	if r.Method == "POST" {
-		r.ParseForm()
-		name, description, priority := r.PostFormValue("name"), r.PostFormValue("description"), r.PostFormValue("priority")
-		date := time.Now().Format("01-02-2006")
-		insForm, err := db.Prepare("INSERT INTO Issues(name, description, priority, date, project_id) VALUES(?,?,?,?,?)")
-		if err != nil {
-			panic(err.Error())
+	if uid != 0 {
+		db := dbConn()
+		if r.Method == "POST" {
+			r.ParseForm()
+			name, description, priority := r.PostFormValue("name"), r.PostFormValue("description"), r.PostFormValue("priority")
+			date := time.Now().Format("01-02-2006")
+			insForm, err := db.Prepare("INSERT INTO Issues(name, description, priority, date, project_id, user_id) VALUES(?,?,?,?,?, ?)")
+			if err != nil {
+				panic(err.Error())
+			}
+			insForm.Exec(name, description, priority, date, project_id, uid)
+			log.Println("INSERT: Name: " + name + " | Description: " + description + " | Priority: " + priority + " | Date: " + date)
 		}
-		insForm.Exec(name, description, priority, date, project_id)
-		log.Println("INSERT: Name: " + name + " | Description: " + description + " | Priority: " + priority + " | Date: " + date)
+		defer db.Close()
+		http.Redirect(w, r, "/index", 301)
+	} else {
+		http.Redirect(w, r, "/", 301)
 	}
-	defer db.Close()
-	http.Redirect(w, r, "/index", 301)
 }
 
 // Update is the router function that updates an existing Blip in the database
 func Update(w http.ResponseWriter, r *http.Request) {
-	db := dbConn()
-	if r.Method == "POST" {
-		r.ParseForm()
-		name, description, priority := r.PostFormValue("name"), r.PostFormValue("description"), r.PostFormValue("priority")
-		id := r.FormValue("project_id")
-		date := time.Now().Format("01-02-2006")
-		insForm, err := db.Prepare("UPDATE Issues SET name=?, description=?, priority=?, date=? WHERE id=?")
-		if err != nil {
-			fmt.Println(err.Error())
+	if uid != 0 {
+		db := dbConn()
+		if r.Method == "POST" {
+			r.ParseForm()
+			name, description, priority := r.PostFormValue("name"), r.PostFormValue("description"), r.PostFormValue("priority")
+			id := r.FormValue("project_id")
+			date := time.Now().Format("01-02-2006")
+			insForm, err := db.Prepare("UPDATE Issues SET name=?, description=?, priority=?, date=? WHERE id=?")
+			if err != nil {
+				fmt.Println(err.Error())
+			}
+			insForm.Exec(name, description, priority, date, id)
+			log.Println("UPDATE: Name: " + name + " | Description: " + description + " | Priority: " + priority + " | Date: " + date)
 		}
-		insForm.Exec(name, description, priority, date, id)
-		log.Println("UPDATE: Name: " + name + " | Description: " + description + " | Priority: " + priority + " | Date: " + date)
+		defer db.Close()
+		http.Redirect(w, r, "/index", 301)
+	} else {
+		http.Redirect(w, r, "/", 301)
 	}
-	defer db.Close()
-	http.Redirect(w, r, "/index", 301)
 }
 
 // Delete is a function that removes the Blip from the database
 func Delete(w http.ResponseWriter, r *http.Request) {
-	db := dbConn()
-	emp := r.URL.Query().Get("id")
-	fmt.Println(r.Method)
-	delForm, err := db.Prepare("DELETE FROM Issues WHERE id=?")
-	if err != nil {
-		panic(err.Error())
+	if uid != 0 {
+		db := dbConn()
+		emp := r.URL.Query().Get("id")
+		fmt.Println(r.Method)
+		delForm, err := db.Prepare("DELETE FROM Issues WHERE id=?")
+		if err != nil {
+			panic(err.Error())
+		}
+		delForm.Exec(emp)
+		log.Println("DELETE")
+		defer db.Close()
+		http.Redirect(w, r, "/index", 301)
+	} else {
+		http.Redirect(w, r, "/", 301)
 	}
-	delForm.Exec(emp)
-	log.Println("DELETE")
-	defer db.Close()
-	http.Redirect(w, r, "/index", 301)
 }
 
 func main() {
@@ -409,6 +501,7 @@ func main() {
 	http.HandleFunc("/signup", SingUpPage)
 	http.HandleFunc("/register", Register)
 	http.HandleFunc("/dashboard", Dashboard)
+	http.HandleFunc("/bugs", DisplayIssues)
 	http.HandleFunc("/showproject", ShowProject)
 	http.HandleFunc("/newproject", NewProject)
 	http.HandleFunc("/editproject", EditProject)
